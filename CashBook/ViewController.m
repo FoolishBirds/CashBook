2d60d829315b419dbcfc650615c2a28ff09e8dfc
tian <984636131@qq.com>
2017-05-24 12:42:06 +0800

    Bird_1

--- CashBook/ViewController.m
+++ CashBook/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"test svn1");
+    NSLog(@"test svnBird");
+//    Bird
 }
 
 
